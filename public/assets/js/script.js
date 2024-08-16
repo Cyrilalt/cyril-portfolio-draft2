7a32fdc11c4d828fdf03d1703b33a3a82ddb00ef
Cyrilalt <123979927+Cyrilalt@users.noreply.github.com>
2024-08-16 01:20:18 +0100

    Update script.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -61,19 +61,16 @@ var swiper = new Swiper(".mySwiper", {
 
 //card slides
 var swiper = new Swiper(".cardSwipper", {
-  effect: "coverflow",
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: "auto",
-      coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-      },
+ slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
       pagination: {
         el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
       },
 });
 
